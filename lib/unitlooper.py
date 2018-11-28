814c38a259b683b10f0b670dc80980f3076e52df
Artanis <kein0011@naver.com>
2018-11-28 20:58:24 +0900

    unitlooper uses eudx, LoopPUnit accepts EUDVariable

--- lib/unitlooper.py
+++ lib/unitlooper.py
@@ -1,133 +1,135 @@
 from eudplib import *
 
+try:
+    from advrw import f_dwepdcunitread_epd_safe
+except (ImportError):
+    @EUDFunc
+    def f_dwepdcunitread_epd_safe(targetplayer):
+        # from advanced read/write functions
+        # https://github.com/armoha/euddraftAddons/raw/master/lib/advrw.zip
 
-@EUDFunc
-def f_dwepdcunitread_epd_safe(targetplayer):
-    # from advanced read/write functions
-    # https://github.com/armoha/euddraftAddons/raw/master/lib/advrw.zip
+        ret, retepd = EUDVariable(), EUDVariable()
 
-    ret, retepd = EUDVariable(), EUDVariable()
+        # Common comparison rawtrigger
+        PushTriggerScope()
+        cmpc = Forward()
+        cmp_player = cmpc + 4
+        cmp_number = cmpc + 8
+        cmpact = Forward()
 
-    # Common comparison rawtrigger
-    PushTriggerScope()
-    cmpc = Forward()
-    cmp_player = cmpc + 4
-    cmp_number = cmpc + 8
-    cmpact = Forward()
-
-    cmptrigger = Forward()
-    cmptrigger << RawTrigger(
-        conditions=[
-            cmpc << Memory(0, AtMost, 0)
-        ],
-        actions=[
-            cmpact << SetMemory(cmptrigger + 4, SetTo, 0)
-        ]
-    )
-    cmpact_ontrueaddr = cmpact + 20
-    PopTriggerScope()
-
-    # static_for
-    chain1 = [Forward() for _ in range(11)]
-    chain2 = [Forward() for _ in range(11)]
-
-    # Main logic start
-    error = 1
-    SeqCompute([
-        (EPD(cmp_player), SetTo, targetplayer),
-        (EPD(cmp_number), SetTo, 0x59CCA8 + 336 * (0x7FF - error)),
-        (ret, SetTo, 0x59CCA8 + 336 * (0x7FF - error)),
-        (retepd, SetTo, EPD(0x59CCA8) + 84 * (0x7FF - error))
-    ])
-
-    readend = Forward()
-
-    for i in range(10, -1, -1):
-        nextchain = chain1[i - 1] if i > 0 else readend
-        epdsubact = [retepd.AddNumber(-84 * 2 ** i)]
-        epdaddact = [retepd.AddNumber(84 * 2 ** i)]
-
-        chain1[i] << RawTrigger(
-            nextptr=cmptrigger,
+        cmptrigger = Forward()
+        cmptrigger << RawTrigger(
+            conditions=[
+                cmpc << Memory(0, AtMost, 0)
+            ],
             actions=[
-                SetMemory(cmp_number, Subtract, 336 * 2 ** i),
-                SetNextPtr(cmptrigger, chain2[i]),
-                SetMemory(cmpact_ontrueaddr, SetTo, nextchain),
-                ret.SubtractNumber(336 * 2 ** i),
-            ] + epdsubact
+                cmpact << SetMemory(cmptrigger + 4, SetTo, 0)
+            ]
+        )
+        cmpact_ontrueaddr = cmpact + 20
+        PopTriggerScope()
+
+        # static_for
+        chain1 = [Forward() for _ in range(11)]
+        chain2 = [Forward() for _ in range(11)]
+
+        # Main logic start
+        error = 1
+        SeqCompute([
+            (EPD(cmp_player), SetTo, targetplayer),
+            (EPD(cmp_number), SetTo, 0x59CCA8 + 336 * (0x7FF - error)),
+            (ret, SetTo, 0x59CCA8 + 336 * (0x7FF - error)),
+            (retepd, SetTo, EPD(0x59CCA8) + 84 * (0x7FF - error))
+        ])
+
+        readend = Forward()
+
+        for i in range(10, -1, -1):
+            nextchain = chain1[i - 1] if i > 0 else readend
+            epdsubact = [retepd.AddNumber(-84 * 2 ** i)]
+            epdaddact = [retepd.AddNumber(84 * 2 ** i)]
+
+            chain1[i] << RawTrigger(
+                nextptr=cmptrigger,
+                actions=[
+                    SetMemory(cmp_number, Subtract, 336 * 2 ** i),
+                    SetNextPtr(cmptrigger, chain2[i]),
+                    SetMemory(cmpact_ontrueaddr, SetTo, nextchain),
+                    ret.SubtractNumber(336 * 2 ** i),
+                ] + epdsubact
+            )
+
+            chain2[i] << RawTrigger(
+                actions=[
+                    SetMemory(cmp_number, Add, 336 * 2 ** i),
+                    ret.AddNumber(336 * 2 ** i),
+                ] + epdaddact
+            )
+
+        readend << NextTrigger()
+
+        RawTrigger(
+            conditions=ret.AtMost(0x59CCA7),
+            actions=[
+                ret.SetNumber(0),
+                retepd.SetNumber(0),
+            ]
+        )
+        RawTrigger(
+            conditions=ret.AtLeast(0x628299),
+            actions=[
+                ret.SetNumber(0),
+                retepd.SetNumber(0),
+            ]
         )
 
-        chain2[i] << RawTrigger(
-            actions=[
-                SetMemory(cmp_number, Add, 336 * 2 ** i),
-                ret.AddNumber(336 * 2 ** i),
-            ] + epdaddact
-        )
+        return ret, retepd
 
-    readend << NextTrigger()
+try:
+    from eudx import MemoryXEPD, SetMemoryXEPD
+except (ImportError):
+    class ConditionX(Condition):
 
-    RawTrigger(
-        conditions=ret.AtMost(0x59CCA7),
-        actions=[
-            ret.SetNumber(0),
-            retepd.SetNumber(0),
-        ]
-    )
-    RawTrigger(
-        conditions=ret.AtLeast(0x628299),
-        actions=[
-            ret.SetNumber(0),
-            retepd.SetNumber(0),
-        ]
-    )
+        def __init__(self, locid, player, amount, unitid,
+                    comparison, condtype, restype, flags):
+            super().__init__(locid, player, amount, unitid,
+                            comparison, condtype, restype, flags)
 
-    return ret, retepd
+            self.fields = [locid, player, amount, unitid,
+                        comparison, condtype, restype, flags,
+                        b2i2(b'SC')]
 
 
-@EUDFunc
-def f_bread2_epd(epd, subp):
-    oldcp = f_getcurpl()
-    b = EUDVariable()
-    addact = Forward()
-    addact_number = addact + 20
-    DoActions([
-        SetCurrentPlayer(epd),
-        b.SetNumber(0),
-        SetMemory(addact_number, SetTo, 0),
-    ])
-    EUDSwitch(subp)
-    for i in range(4):
-        EUDSwitchCase()(i)
-        for j in range(31, -1, -1):
-            if 8 * i <= j < 8 * (i + 1):
-                RawTrigger(
-                    conditions=Deaths(CurrentPlayer, AtLeast, 2**j, 0),
-                    actions=[
-                        SetDeaths(CurrentPlayer, Subtract, 2**j, 0),
-                        SetMemory(addact_number, Add, 2**j),
-                        b.AddNumber(2**j)
-                    ]
-                )
-            else:
-                RawTrigger(
-                    conditions=Deaths(CurrentPlayer, AtLeast, 2**j, 0),
-                    actions=[
-                        SetDeaths(CurrentPlayer, Subtract, 2**j, 0),
-                        SetMemory(addact_number, Add, 2**j),
-                    ]
-                )
-            if j == 8 * i:
-                break
-        EUDBreak()
-    EUDEndSwitch()
-    DoActions([
-        addact << SetDeaths(CurrentPlayer, Add, 0xEDAC, 0),
-        SetCurrentPlayer(oldcp),
-    ])
-    return b
+    def DeathsX(Player, Comparison, Number, Unit, Mask):
+        Player = EncodePlayer(Player, issueError=True)
+        Comparison = EncodeComparison(Comparison, issueError=True)
+        Unit = EncodeUnit(Unit, issueError=True)
+        return ConditionX(Mask, Player, Number, Unit, Comparison, 15, 0, 0)
 
 
-def LoopNewUnit():
+    def MemoryXEPD(dest, cmptype, value, mask):
+        return DeathsX(dest, cmptype, value, 0, mask)
+
+
+    class ActionX(Action):
+
+        def __init__(self, locid1, strid, wavid, time, player1, player2,
+                    unitid, acttype, amount, flags):
+            super().__init__(locid1, strid, wavid, time, player1, player2,
+                            unitid, acttype, amount, flags)
+
+            self.fields = [locid1, strid, wavid, time, player1,
+                        player2, unitid, acttype, amount, flags,
+                        0, b2i2(b'SC')]
+
+
+    def SetMemoryXEPD(dest, modtype, value, mask):
+        dest = EncodePlayer(dest, issueError=True)
+        modtype = EncodeModifier(modtype, issueError=True)
+        return ActionX(mask, 0, 0, 0, dest, value, 0, 45, modtype, 20)
+
+
+def LoopNewUnit(allowance=2):
     firstUnitPtr = EPD(0x628430)
     EUDCreateBlock('newunitloop', 'newlo')
     tos0 = EUDLightVariable()
@@ -135,13 +137,12 @@ def LoopNewUnit():
 
     ptr, epd = f_dwepdcunitread_epd_safe(firstUnitPtr)
     if EUDWhile()(ptr >= 1):
-        targetOrderSpecial = f_bread2_epd(epd + 0xA5 // 4, 1)
-        if EUDIf()(targetOrderSpecial >= 0x100):
-            f_dwsubtract_epd(epd + 0xA5 // 4, targetOrderSpecial)
+        if EUDIf()(MemoryXEPD(epd + 0xA5 // 4, AtLeast, 0x100, 0xFF00)):
+            DoActions(SetMemoryXEPD(epd + 0xA5 // 4, SetTo, 0, 0xFF00))
             yield ptr, epd
         if EUDElse()():
             DoActions(tos0.AddNumber(1))
-            EUDBreakIf(tos0.AtLeast(2))
+            EUDBreakIf(tos0.AtLeast(allowance))
         EUDEndIf()
         EUDSetContinuePoint()
         SetVariables([ptr, epd], f_dwepdcunitread_epd_safe(epd + 1))
@@ -211,13 +212,13 @@ def CPLoopUnit():
 
 
 def LoopPUnit(player_number):
-    if player_number == CurrentPlayer or player_number == EncodePlayer(CurrentPlayer):
-        player_number = f_getcurpl()
     if isUnproxyInstance(player_number, type(P1)):
         player_number = EncodePlayer(player_number)
-    firstPlayerUnitPtr = 0x6283F8 + 4 * player_number
+        if player_number == EncodePlayer(CurrentPlayer):
+            player_number = f_getcurpl()
+    firstPlayerUnitPtr = 0x6283F8
     EUDCreateBlock('playerunitloop', firstPlayerUnitPtr)
-    ptr, epd = f_dwepdcunitread_epd_safe(EPD(firstPlayerUnitPtr))
+    ptr, epd = f_dwepdcunitread_epd_safe(EPD(firstPlayerUnitPtr) + player_number)
 
     if EUDWhile()(ptr >= 1):
         yield ptr, epd
