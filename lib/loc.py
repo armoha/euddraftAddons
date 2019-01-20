56546d1681d93066528cf18e61f14b9fe5949952
armoha <kein0011@naver.com>
2019-01-21 01:46:44 +0900

    loc func optimization

--- lib/loc.py
+++ lib/loc.py
@@ -1,47 +1,72 @@
 from eudplib import *
 
 
-@EUDFunc
+def _locfgen(mod1, mod2, mod3, mod4):
+
+    @EUDFunc
+    def _locf(epd, x, y):
+        act = Forward()
+
+        VProc(epd, [
+            epd.AddNumber(EPD(0x58DC60)),
+            epd.QueueAssignTo(EPD(act) + 4)
+        ])
+        VProc(x, x.QueueAssignTo(EPD(act) + 5))
+
+        VProc(epd, [
+            epd.AddNumber(1),
+            SetMemory(epd._varact + 16, Add, 8)
+        ])
+        VProc(y, y.QueueAssignTo(EPD(act) + 5 + 8))
+
+        VProc(epd, [
+            epd.AddNumber(1),
+            SetMemory(epd._varact + 16, Add, 8)
+        ])
+        VProc(x, SetMemory(x._varact + 16, Add, 16))
+
+        VProc(epd, [
+            epd.AddNumber(1),
+            SetMemory(epd._varact + 16, Add, 8)
+        ])
+        VProc(y, SetMemory(y._varact + 16, Add, 16))
+
+        DoActions([
+            act << SetMemory(0, mod1, 0),
+            SetMemory(0, mod2, 0),
+            SetMemory(0, mod3, 0),
+            SetMemory(0, mod4, 0),
+        ])
+
+    return _locf
+
+
+_SetLoc = _locfgen(SetTo, SetTo, SetTo, SetTo)
+_AddLoc = _locfgen(Add, Add, Add, Add)
+_ResizeLoc = _locfgen(Subtract, Subtract, Add, Add)
+
+
 def SetLoc(locID, x, y):
-    DoActions([
-        SetMemoryEPD(EPD(0x58DC60) + 5 * locID, SetTo, x),
-        SetMemoryEPD(EPD(0x58DC60) + 5 * locID + 1, SetTo, y),
-        SetMemoryEPD(EPD(0x58DC60) + 5 * locID + 2, SetTo, x),
-        SetMemoryEPD(EPD(0x58DC60) + 5 * locID + 3, SetTo, y),
-    ])
+    _SetLoc(locID * 5, x, y)
 
 
-@EUDFunc
 def AddLoc(locID, x, y):
-    DoActions([
-        SetMemoryEPD(EPD(0x58DC60) + 5 * locID, Add, x),
-        SetMemoryEPD(EPD(0x58DC60) + 5 * locID + 1, Add, y),
-        SetMemoryEPD(EPD(0x58DC60) + 5 * locID + 2, Add, x),
-        SetMemoryEPD(EPD(0x58DC60) + 5 * locID + 3, Add, y),
-    ])
+    _AddLoc(locID * 5, x, y)
 
 
-@EUDFunc
 def ResizeLoc(locID, x, y):
-    DoActions([
-        SetMemoryEPD(EPD(0x58DC60) + 5 * locID, Subtract, x),
-        SetMemoryEPD(EPD(0x58DC60) + 5 * locID + 1, Subtract, y),
-        SetMemoryEPD(EPD(0x58DC60) + 5 * locID + 2, Add, x),
-        SetMemoryEPD(EPD(0x58DC60) + 5 * locID + 3, Add, y),
-    ])
+    _ResizeLoc(locID * 5, x, y)
 
 
 @EUDFunc
-def f_getLocTL(locID):
+def _GetLocTL(epd):
+    epd += EPD(0x58DC60)
+    EUDReturn(
+        f_dwread_epd(epd),
+        f_dwread_epd(epd + 1))
+
+
+def GetLocTL(locID):
     # 로케이션의 위(top), 왼쪽 (left) 좌표를 얻어냅니다.
     # @param  {[type]} locID 로케이션 번호. $L(로케이션 이름) 으로 얻을 수 있습니다.
-    EUDReturn(
-        f_dwread_epd(EPD(0x58DC60) + 5 * locID + 0),
-        f_dwread_epd(EPD(0x58DC60) + 5 * locID + 1))
-
-
-@EUDFunc
-def SetLocEPD(locID, epd):
-    xy = f_dwread_epd(epd)
-    x, y = f_dwbreak(xy)[0:2]
-    SetLoc(locID, x, y)
+    return _GetLocTL(locID * 5)
