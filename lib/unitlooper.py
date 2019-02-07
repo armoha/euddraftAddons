db8657056e964ce5aa2c11224eec10ba4aaa0861
armoha <kein0011@naver.com>
2019-02-07 17:56:41 +0900

    Wrap two actions into one trigger.

--- lib/unitlooper.py
+++ lib/unitlooper.py
@@ -237,17 +237,19 @@ def EUDLoopUnit2():
     1700개 유닛을 도는 방식으로 작동합니다.
     """
     ptr, epd = EUDCreateVariables(2)
-    ptr << 0x59CCA8
-    epd << EPD(0x59CCA8)
+    DoActions([
+        ptr.SetNumber(0x59CCA8),
+        epd.SetNumber(EPD(0x59CCA8)),
+    ])
     if EUDLoopN()(1700):
-        ptr2, epd2 = EUDCreateVariables(2)
-        SetVariables([ptr2, epd2], [ptr, epd])
         # sprite가 NULL이면 없는 유닛으로 판단.
         EUDContinueIf(MemoryEPD(epd + (0x0C // 4), Exactly, 0))
         yield ptr, epd
         EUDSetContinuePoint()
-        ptr += 336
-        epd += 336 // 4
+        DoActions([
+            ptr.AddNumber(336),
+            epd.AddNumber(336 // 4),
+        ])
     EUDEndLoopN()
 
 
